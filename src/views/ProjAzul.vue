a1e4abba34ea0544972c365fad05d1701656352e
rafaelabarbieric <barbieridacruzrafaela@gmail.com>
2024-04-07 12:34:27 -0300

    feat: criação dos projetos #3

--- src/views/ProjAzul.vue
+++ src/views/ProjAzul.vue
@@ -1,10 +1,36 @@
 <script setup>
+
 </script>
 
 <template>
-    <div>
+    <main>
+        <div id="projAzul">
+            <h1>Projeto Azul</h1>
+            <h2>from Rafaela Barbieri</h2>
+        </div>
+    </main>
 
-        <h1>This is a projeto azul</h1>
-
-    </div>
 </template>
+
+<style scoped>
+    #projAzul{
+        width: max-content;
+    }
+    h1{
+        font-size: 130px;
+        font-family: "Katibeh", serif;
+        font-weight:bold;
+        line-height: 0.8;
+        letter-spacing: 3px;
+        color: #2A359F;
+    }
+    h2{
+        font-size:70px;
+        font-family: "Katibeh", serif;
+        font-weight: bold;
+        line-height: 0.8;
+        color: black;
+        letter-spacing: 1px;
+        text-align:end;
+    }
+</style>
